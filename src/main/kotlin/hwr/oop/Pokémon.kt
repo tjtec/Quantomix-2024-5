913e6d413210a3c61fb14adfdf2445e8d1e0c8c9
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-04-03 15:54:44 +0200

    add function to set pokémon variables

--- src/main/kotlin/hwr/oop/Pokémon.kt
+++ src/main/kotlin/hwr/oop/Pokémon.kt
@@ -10,14 +10,21 @@ class Pokémon () {
     var specialAttack: Int = 0
     var specialDefense: Int = 0
     var speed: Int = 0
-    fun pokémon (pokename:String):Pokémon {
-
-    }
-    fun set_Pokémon_variables(){
 
 
+    fun set_pokémon_variables (pokename:String, typ1:String, typ2:String, input_kp: String, att:String, def:String, sAtt:String, sDef:String) {
+        name=pokename
+        attackType1=typ1
+        attackType2=typ2
+        kp=Integer.valueOf(input_kp)
+        defense=Integer.valueOf(def)
+        attack=Integer.valueOf(att)
+        specialAttack=Integer.valueOf(sAtt)
+        specialDefense=Integer.valueOf(def)
     }
 }
 
-fun addNewPokémon (name: String) {
+fun addNewPokémon (name: String): Pokémon{
+    val newPokémon=Pokémon()
+
 }
