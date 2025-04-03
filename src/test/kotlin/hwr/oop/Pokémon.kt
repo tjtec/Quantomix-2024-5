dedcb3eccf451bc91645d27591f951241bfbffcf
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-04-03 14:20:30 +0200

    add class Pokémon

---
+++ src/test/kotlin/hwr/oop/Pokémon.kt
@@ -0,0 +1,18 @@
+package hwr.oop
+
+import io.kotest.core.source.SourceRef
+
+class Pokémon (val name: String) {
+    var attackType1: String = ""
+    var attackType2: String = ""
+    var kp: Int = 0
+    var attack: Int = 0
+    var defense: Int = 0
+    var specialAttack: Int = 0
+    var specialDefense: Int = 0
+    var speed: Int = 0
+}
+
+fun addNewPokémon (name: String) {
+    // ToDo: add the list in
+}
