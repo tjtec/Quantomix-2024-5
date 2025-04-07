f87e9e4cc680a3658e832fa082a813e140b281e7
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-04-07 14:07:13 +0200

    test quantomix and delete name pokémon

--- src/main/kotlin/hwr/oop/DBHandler.kt
+++ src/main/kotlin/hwr/oop/DBHandler.kt
@@ -1,9 +1,10 @@
 package hwr.oop
 
 import java.io.File
+import java.lang.Integer.parseInt
 
 class DBHandler {
-    fun getMonsterbyName(file: File, Name: String): Boolean  {
+    fun getMonsterbyName(file: File, Name: String): Boolean {
         val seperator = ",";
         var searchSuccessful = false;
         try {
@@ -33,47 +34,5 @@ class DBHandler {
             println("Fehler beim Lesen der Datei: ${e.localizedMessage}")
         }
         return searchSuccessful;
-fun getMonsterbyName(file: File, Name: String, monster: Pokemon) {
-    val seperator = ",";
-    try {
-        file.forEachLine { line ->
-            val block = line.split(seperator);
-            val mName = block[0];
-            //println(mName);
-            if (block[0] == Name) {
-                var type2 = block[2].toString();
-                if (type2 == "") {
-                    type2 = "none";
-                }
-                //ALL Values for Entity
-                monster.setPokemonVariables(block[0],block[1],block[2],block[3],block[4],block[5],block[6], block[7], block[8]);
-//                println("name:" + block[0])
-//                println("attackType1:" + block[1]);
-//                println("attackType2:" + type2);
-//                println("kp:" + block[3]);
-//                println("attack:" + block[4]);
-//                println("defense:" + block[5]);
-//                println("specialAttack:" + block[6]);
-//                println("specialDefense:" + block[7]);
-//                println("speed:" + block[8]);
-            }
-        }
-    } catch (e: Exception) {
-        println("Fehler beim Lesen der Datei: ${e.localizedMessage}")
-    }
-}
-
-fun main(args: Array<String>) {
-    val monsterDB = File("src/main/kotlin/hwr/oop/resources/test.csv");
-
-    if (monsterDB.exists()) {
-        if(args.size > 0) { //Suche über Commandline args möglich
-            DBHandler().getMonsterbyName(monsterDB, args[0]);
-        }
-        else{ //Usecase ohne Commandline
-            DBHandler().getMonsterbyName(monsterDB, "Glurak");
-        }
-    } else {
-        println("Die Datei '${monsterDB.name}' wurde nicht gefunden.")
     }
 }
--- src/test/kotlin/hwr/oop/KotlinExampleTest.kt
+++ src/test/kotlin/hwr/oop/KotlinExampleTest.kt
@@ -15,4 +15,17 @@ class KotlinExampleTest : AnnotationSpec() {
     val monsterDB = File("src/main/kotlin/hwr/oop/resources/test.csv");
     assertThat(DBHandler().getMonsterbyName(monsterDB, "Glurak")).isEqualTo(true)
   }
+  @Test
+  fun `Quantomix test` () {
+    val quantomix: Quantomix = Quantomix("Test","hello","world", 200, 100, 30, 70, 35, 88)
+    assertThat(quantomix.quantomixName).isEqualTo("Test")
+    assertThat(quantomix.typ1).isEqualTo("hello")
+    assertThat(quantomix.typ2).isEqualTo("world")
+    assertThat(quantomix.inputKp).isEqualTo(200)
+    assertThat(quantomix.attack).isEqualTo(100)
+    assertThat(quantomix.defense).isEqualTo(30)
+    assertThat(quantomix.specialAttack).isEqualTo(70)
+    assertThat(quantomix.specialDefense).isEqualTo(35)
+    assertThat(quantomix.speed).isEqualTo(88)
+  }
 }
