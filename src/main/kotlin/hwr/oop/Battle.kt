f0d578f3c177445aceaf52e763868f9fd07017a6
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-04-08 12:59:49 +0200

    beginning to implement battle logic

--- src/main/kotlin/hwr/oop/Battle.kt
+++ src/main/kotlin/hwr/oop/Battle.kt
@@ -2,34 +2,29 @@ package hwr.oop
 
 class Battle(var quantomixA: Quantomix, var quantomixB: Quantomix) {
 
-    fun nextAttacker (): Quantomix{
-        if (quantomixA.speed >= quantomixB.speed){
+    fun nextAttacker(): Quantomix {
+        if (quantomixA.speed >= quantomixB.speed) {
             return quantomixA;
-        }
-        else {
+        } else {
             return quantomixB;
         }
     }
 
-    fun attackPower (attack:Attack): Int {
+    fun attackPower(attack: Attack): Int {
         //ToDo: überlege eine bessere Berechnung
         //ToDo: Attacken ohne Schaden miteinbeziehen?
-        var attackPower: Int=0
-        if (nextAttacker()==quantomixA){
-            var otherQuantomix: Quantomix = quantomixA
+        var attackPower: Int = 0
+        val otherQuantomix = if (nextAttacker() == quantomixA) {
+            quantomixA
+        } else {
+            quantomixB
         }
-        else {
-            var otherQuantomix: Quantomix = quantomixB
-        }
-        if (attack.type == nextAttacker().typ1){
-            val otherQuantomix
-            attackPower=attack.damage*nextAttacker().specialAttack/otherQuantomix.specialDefense
-        }
-        else if (attack.type == nextAttacker().typ2){
-            attackPower=attack.damage*nextAttacker().specialAttack/otherQuantomix.specialDefense
-        }
-        else {
-            attackPower=attack.damage*nextAttacker().attack/otherQuantomix.defense
+        if (attack.type == nextAttacker().typ1) {
+            attackPower = attack.damage * nextAttacker().specialAttack / otherQuantomix.specialDefense
+        } else if (attack.type == nextAttacker().typ2) {
+            attackPower = attack.damage * nextAttacker().specialAttack / otherQuantomix.specialDefense
+        } else {
+            attackPower = attack.damage * nextAttacker().attack / otherQuantomix.defense
         }
         return attackPower
     }
