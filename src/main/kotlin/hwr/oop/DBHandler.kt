5832e944ef498a7f70dd865882d0742f2ba6c94e
Tim TJTec <tim@haist.eu>
2025-04-08 11:14:24 +0200

    Changed Arrays to list

--- src/main/kotlin/hwr/oop/DBHandler.kt
+++ src/main/kotlin/hwr/oop/DBHandler.kt
@@ -4,53 +4,22 @@ import java.io.File
 import java.lang.Integer.parseInt
 
 class DBHandler {
-    fun getMonsterbyName(file: File, Name: String): Boolean {
-        val seperator = ",";
-        var searchSuccessful = false;
-        try {
-            file.forEachLine { line ->
-                val block = line.split(seperator);
-                val mName = block[0];
-                //println(mName);
-                if (block[0] == Name) {
-                    var type2 = block[2].toString();
-                    if (type2 == "") {
-                        type2 = "none";
-                    }
-                    //ALL Values for Entity
-                    println("name:" + block[0])
-                    println("attackType1:" + block[1]);
-                    println("attackType2:" + type2);
-                    println("kp:" + block[3]);
-                    println("attack:" + block[4]);
-                    println("defense:" + block[5]);
-                    println("specialAttack:" + block[6]);
-                    println("specialDefense:" + block[7]);
-                    println("speed:" + block[8]);
-                    searchSuccessful = true;
-                }
-            }
-        } catch (e: Exception) {
-            println("Fehler beim Lesen der Datei: ${e.localizedMessage}")
-        }
-        return searchSuccessful;
-    }
 
     fun getMonsterbyNameObject(file: File, Name: String): Quantomix {
         val seperator = ",";
         var searchSuccessful = false;
-        var returnArray: Array<String>? = null;
+        var returnArray: List<String>? = null;
         try {
             file.forEachLine { line ->
-                val block = line.split(seperator);
-                val mName = block[0];
+                val block = line.split(seperator).toList();
+                val mName = block[0].toList();
                 if (block[0] == Name) {
                     var type2 = block[2].toString();
                     if (type2 == "") {
                         type2 = "none";
                     }
                     //ALL Values for Entity
-                    returnArray = arrayOf<String>(
+                    returnArray = listOf(
                         block[0],
                         block[1],
                         type2,
