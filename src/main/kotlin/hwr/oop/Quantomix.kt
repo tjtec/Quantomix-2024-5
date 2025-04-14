20f0a4cc860d92c1f05e1bc527869c8e60082bf8
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-04-14 09:27:49 +0200

    battle logic without effectivity

--- src/main/kotlin/hwr/oop/Quantomix.kt
+++ src/main/kotlin/hwr/oop/Quantomix.kt
@@ -4,7 +4,7 @@ class Quantomix(
     val quantomixName: String,
     val typ1: String,
     val typ2: String,
-    val inputKp: Int,
+    var inputKp: Int,
     val attack: Int,
     val defense: Int,
     val specialAttack: Int,
