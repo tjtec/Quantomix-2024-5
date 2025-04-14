65dca758cea655f77238c3eec94dbfa894e779a5
Tim TJTec <tim@haist.eu>
2025-04-14 14:17:05 +0200

    New CSV Parser

--- src/main/kotlin/hwr/oop/DBHandler.kt
+++ src/main/kotlin/hwr/oop/DBHandler.kt
@@ -5,38 +5,61 @@ import java.lang.Integer.parseInt
 
 class DBHandler {
 
-    fun createQuantomixObject(file: File, Name: String): Quantomix {
+    fun getData(file: File, column: Int = -1, row: Int = -1, query: String = "" ): List<String> {
         val seperator = ",";
-        var searchSuccessful = false;
-        var returnArray: List<String>? = null;
+        var currentline = -1;
+        var returnList: List<String>? = null;
+        println("File: ${file.absolutePath}")
         try {
             file.forEachLine { line ->
+                currentline += 1;
                 val block = line.split(seperator).toList();
-                val mName = block[0].toList();
-                if (block[0] == Name) {
-                    var type2 = block[2].toString();
-                    if (type2 == "") {
-                        type2 = "none";
+                if (column != -1 && query != "") { //Wenn eine Suchspalte und Suchkriterium angegeben wurde.
+                    if (block[column] == query) {
+                        returnList = block;
                     }
-                    //ALL Values for Entity
-                    returnArray = listOf(
-                        block[0],
-                        block[1],
-                        type2,
-                        block[3],
-                        block[4],
-                        block[5],
-                        block[6],
-                        block[7],
-                        block[8]
-                    );
-                    searchSuccessful = true;
+                }
+                else if (row != -1 && column == -1) { // Wenn nur Zeile angegeben.
+                    if (currentline == row){
+                        returnList = block;
+                    }
+                }
+                else if (column != -1 && row != -1) { //Wenn Zeile und Spalte angegeben.
+                    if (currentline == row){
+                        returnList = listOf(block[column]);
+                    }
+                }
+                else {
+                    error("Invalid Arguments.");
                 }
             }
-        } catch (e: Exception) {
-            //println("Fehler beim Lesen der Datei: ${e.localizedMessage}")
-            error("Fehler beim Lesen der Datei")
+            return returnList!!;
         }
+        catch (e: Exception) {
+        println("Fehler beim Lesen der Datei: ${e.localizedMessage}")
+        error("Fehler beim Lesen der Datei")
+        }
+    }
+
+    fun createQuantomixObject(file: File, Name: String): Quantomix {
+        var block = getData(file,0,-1,Name);
+        var returnArray: List<String>? = null;
+        var type2 = block[2].toString();
+        if (type2 == "") {
+            type2 = "none";
+        }
+        //ALL Values for Entity
+        returnArray = listOf(
+            block[0],
+            block[1],
+            type2,
+            block[3],
+            block[4],
+            block[5],
+            block[6],
+            block[7],
+            block[8]
+            );
         if (returnArray != null) {
             var returnQuantomix: Quantomix = Quantomix(
                 returnArray!![0],
@@ -57,16 +80,25 @@ class DBHandler {
         }
     }
 }
-//fun main(){
-//    val monsterDB = File("src/main/kotlin/hwr/oop/resources/test.csv");
-//    val temp_quantomix = DBHandler().createQuantomixObject(monsterDB, "Schlatompfe");
-//    println(temp_quantomix.quantomixName);
-//    println(temp_quantomix.typ1);
-//    println(temp_quantomix.typ2);
-//    println(temp_quantomix.inputKp);
-//    println(temp_quantomix.attack);
-//    println(temp_quantomix.defense);
-//    println(temp_quantomix.specialAttack);
-//    println(temp_quantomix.specialDefense);
-//    println(temp_quantomix.speed);
-//}
+fun main(){
+    val monsterDB = File("src/main/resources/test.csv");
+    val effDB = File("src/main/resources/Effektivitaet.csv");
+
+    println("Create Quantomix Object");
+    val temp_quantomix = DBHandler().createQuantomixObject(monsterDB, "Schlatompfe");
+    println(temp_quantomix.quantomixName);
+    println(temp_quantomix.typ1);
+    println(temp_quantomix.typ2);
+    println(temp_quantomix.inputKp);
+    println(temp_quantomix.attack);
+    println(temp_quantomix.defense);
+    println(temp_quantomix.specialAttack);
+    println(temp_quantomix.specialDefense);
+    println(temp_quantomix.speed);
+
+    println("Kampf Gegen Normal Effektivität");
+    println(DBHandler().getData(effDB,Effektivitaet().Klassen.get("Flug")!!,Effektivitaet().Klassen.get("Kampf")!!));
+
+    println("Return Line Array");
+    println(DBHandler().getData(effDB,-1,1,""));
+}
---
+++ src/main/kotlin/hwr/oop/Effektivitaet.kt
@@ -0,0 +1,24 @@
+package hwr.oop
+
+data class Effektivitaet(
+    public var Klassen: Map<String, Int> = mapOf(
+        "Normal" to 1,
+        "Kampf" to 2,
+        "Flug" to 3,
+        "Gift" to 4,
+        "Boden" to 5,
+        "Gestein" to 6,
+        "Käfer" to 7,
+        "Geist" to 8,
+        "Stahl" to 9,
+        "Feuer" to 10,
+        "Wasser" to 11,
+        "Pflanze" to 12,
+        "Elektro" to 13,
+        "Psycho" to 14,
+        "Eis" to 15,
+        "Drache" to 16,
+        "Unlicht" to 17,
+        "Fee" to 18
+    )
+)
