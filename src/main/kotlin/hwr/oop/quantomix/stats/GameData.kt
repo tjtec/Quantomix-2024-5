d90f21afc7261546397f270621d76684837e8f3b
TJTec <drcodehd@gmail.com>
2025-05-05 03:06:16 +0200

    Sorted Classes into Packages, deleted unused classes, fixed CSV stuff

--- src/main/kotlin/hwr/oop/quantomix/stats/GameData.kt
+++ src/main/kotlin/hwr/oop/quantomix/stats/GameData.kt
@@ -3,6 +3,6 @@ package hwr.oop.quantomix.stats
 import java.io.File
 
 class GameData {
-    val monsterDB = File("src/main/resources/test.csv");
+    val monsterDB = File("src/main/resources/quantomix.csv");
     val effDB = File("src/main/resources/Effektivitaet.csv");
 }
