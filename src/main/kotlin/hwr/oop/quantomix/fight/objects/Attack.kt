6db3b57c481f4af66874e814b5f1a423a7f675a3
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-05-05 15:03:15 +0200

    add attribute attack in effectivity

--- src/main/kotlin/hwr/oop/quantomix/fight/objects/Attack.kt
+++ src/main/kotlin/hwr/oop/quantomix/fight/objects/Attack.kt
@@ -1,8 +1,10 @@
 package hwr.oop.quantomix.fight.objects
 
+import hwr.oop.quantomix.objects.Typ
+
 class Attack (
     val attackName: String,
-    val type: String?,
+    val type: Typ,
     val damage: Int?,
     val damageQuote: Int?,
 )
