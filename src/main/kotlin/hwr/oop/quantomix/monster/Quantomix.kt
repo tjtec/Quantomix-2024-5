ca806e977a32b121d14601e018a932d19bc5daff
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-05-08 10:12:14 +0200

    make mutation Tests better

--- src/main/kotlin/hwr/oop/quantomix/monster/Quantomix.kt
+++ src/main/kotlin/hwr/oop/quantomix/monster/Quantomix.kt
@@ -2,6 +2,7 @@ package hwr.oop.quantomix.monster
 
 import hwr.oop.quantomix.fight.logic.BattleStats
 import hwr.oop.quantomix.fight.objects.Attack
+import hwr.oop.quantomix.objects.Coach
 import hwr.oop.quantomix.objects.Typ
 
 class Quantomix(
@@ -15,5 +16,36 @@ class Quantomix(
     val specialDefense: Int,
     val speed: Int,
     val attacks: List<Attack>,
-    var battleStats: BattleStats? = null
+    var battleStats: BattleStats = BattleStats(
+        kp, attack, defense, specialAttack, specialDefense, speed,
+        Quantomix(
+            "Lückenfüller",
+            Typ("Lücke"),
+            null,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            listOf(Attack("Lückenfüller", Typ("Lücke"), null, null))
+        ), attacks[0],
+        Coach(
+            "Lückenfüller",
+            listOf(
+                Quantomix(
+                    "Lückenfüller",
+                    Typ("Lücke"),
+                    null,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    listOf(Attack("Lückenfüller", Typ("Lücke"), null, null))
+                )
+            )
+        )
+    )
 )
