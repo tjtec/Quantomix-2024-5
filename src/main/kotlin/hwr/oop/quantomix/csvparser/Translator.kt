f5b158b962297ef84590282fe986d66bd1d64e82
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-05-12 15:13:22 +0200

    make class to interface

--- src/main/kotlin/hwr/oop/quantomix/csvparser/Translator.kt
+++ src/main/kotlin/hwr/oop/quantomix/csvparser/Translator.kt
@@ -5,7 +5,7 @@ import hwr.oop.quantomix.monster.Quantomix
 import hwr.oop.quantomix.objects.Coach
 import hwr.oop.quantomix.objects.Typ
 
-class Translator() {
+interface Translator {
     fun translateToAttack(
         name: String,
         type: String,
