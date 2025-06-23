931ca2187582efd130ab8e7d593243450cb278e0
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-06-23 17:39:37 +0200

    correct minimal mistake val damageStrategy to var damageStrategy

--- src/main/kotlin/hwr/oop/quantomix/fight/logic/BattleFundamentals.kt
+++ src/main/kotlin/hwr/oop/quantomix/fight/logic/BattleFundamentals.kt
@@ -2,5 +2,5 @@ package hwr.oop.quantomix.fight.logic
 
 object BattleFundamentals {
   var numberOfQuantomixInRound: Int = 1
-  val damageStrategy: DamageStrategy=StandardDamageStrategy()
+  var damageStrategy: DamageStrategy=StandardDamageStrategy()
 }
